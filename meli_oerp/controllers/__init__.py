8166d4bb2a73114a1a5bc952d509e6bcbbf0b7ed
jobiols <jorge.obiols@gmail.com>
2019-10-30 19:17:12 -0300

    version11

--- meli_oerp/controllers/__init__.py
+++ meli_oerp/controllers/__init__.py
@@ -6,7 +6,4 @@
 #
 ##############################################################################
 
-from . import models
-from . import controllers
-#import wizard
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+from . import main
--- meli_oerp/meli_oerp_config.py
+++
@@ -1,7 +0,0 @@
-#meli_oerp_config
-
-REDIRECT_URI = 'https://clientes.glinsar.com/meli_login'
-ACCESS_TOKEN = ''
-REFRESH_TOKEN = ''
-CLIENT_ID = ''
-CLIENT_SECRET = ''
